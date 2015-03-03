7dc05af5b5676c079a99e9796d3113df7ad19eb4
Roberto Diaz <roberto.diaz@upcnet.es>
2015-03-03 08:36:23 +0100

    added title to acta

--- genweb/rectorat/browser/views.py
+++ genweb/rectorat/browser/views.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from upc.genweb.newsletter.browser.newsletter import NewsletterBase
-from datetime import datetime
 
 
 class ActaPrintView(NewsletterBase):
 
     __call__ = ViewPageTemplateFile('views/acta_print.pt')
 
-    def currentDate(self):
-        #import ipdb;ipdb.set_trace()
-        #date = api.portal.get_localized_time(datetime.today())
-        date = datetime.today().strftime("%d/%m/%Y")
-        return date
+    def organGovernTitle(self):
+        # Get organGovern Title used printing the acta
+        return self.aq_parent.aq_parent.aq_parent.Title()
