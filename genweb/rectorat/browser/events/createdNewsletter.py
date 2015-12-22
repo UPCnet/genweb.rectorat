2220fd6df0333c3b5c24d651f0b1adb689460f99
root muntanyeta <root@muntanyeta.upc.edu>
2015-12-22 15:15:57 +0100

    removed bug in path

--- genweb/rectorat/browser/events/createdNewsletter.py
+++ genweb/rectorat/browser/events/createdNewsletter.py
@@ -11,7 +11,7 @@ def createdNewsletter(value, event):
     portal = api.portal.get()
     try:
         # /ca/plantilles-butlletins/plantilla/template-base/
-        template = portal['ca']['plantilles-butlletins']['plantilla']['template-base2']['row']
+        template = portal['ca']['plantilles-butlletins']['plantilla']['template-base']['row']
         api.content.copy(source=template, target=value)
     except:
         value.plone_utils.addPortalMessage(_("No s'ha pogut copiar la plantilla per defecte. El fitxer amb les dades, no existeix: /ca/plantilles-butlletins/plantilla/template-base"), 'error')
